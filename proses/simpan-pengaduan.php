196319eb5ec040f56e69870c3b6a0d4088b44c86
Nyx-code <auliausna6@gmail.com>
2026-02-05 08:27:58 +0700

    mengambil db

--- proses/simpan-pengaduan.php
+++ proses/simpan-pengaduan.php
@@ -1,9 +1,20 @@
 <?php
-session_start();
+    session_start();
 
-include "../koneksi.php";
+    include "../koneksi.php";
 
-if(!isset($_SESSION['siswa'])) {
-    header ("location: ../siswa/login-siswa.php");
-}
+    //proteksi login siswa
+    if(!isset($_SESSION['siswa'])) {
+    header("location: ../siswa/login-siswa.php");
+    exit;
+    }
+
+    //mengambil nis dari database
+    $nis = $_SESSION['siswa']['nis'];
+
+    $id_kategori = mysqli_real_escape_string($conn, $_POST['id_kategori']);
+
+    $lokasi = mysqli_real_escape($conn, $_POST['lokasi']);
+
+    $ket = mysqli_real_escape($conn, $_POST['ket']);
 ?>
